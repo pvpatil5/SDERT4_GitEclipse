728f2239fc64a4f671736af062d86dd34f0e5589
DELL <DELL@DESKTOP-EV7GFU4>
2022-03-08 12:12:41 +0530

    rajendra's branch
    Merge branch 'master' of
    https://github.com/pvpatil5/SDERT4_GitEclipse.git

---
+++ src/test/java/com/Raj1/New1class.java
@@ -0,0 +1,11 @@
+package com.Raj1;
+
+import org.testng.annotations.Test;
+
+public class New1class {
+	@Test
+	public void method() {
+		System.out.println("hgghj");
+	}
+
+}
---
+++ src/test/java/com/Raj1/New2class.java
@@ -0,0 +1,10 @@
+package com.Raj1;
+
+public class New2class {
+
+	public static void main(String[] args) {
+		System.out.println("ghghjg");
+
+	}
+
+}
